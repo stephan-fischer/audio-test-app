039ddbf92948b7282fe72e20772fb3185030e402
Stephan Fischer <stephan@mrfischer.de>
2025-03-11 20:25:03 +0100

    NativeAudio Test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,90 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NativeAudio } from '@capacitor-community/native-audio';
 import { SpeechRecognition } from '@capawesome-team/capacitor-speech-recognition';
 import { SpeechSynthesis, AudioSessionCategory, QueueStrategy } from '@capawesome-team/capacitor-speech-synthesis';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() { }
+    constructor() { }
+
+    async ngOnInit(): Promise<void> {}
 
 
-  async checkPermissions() {
-    const { speechRecognition, audioRecording } = await SpeechRecognition.checkPermissions();
+    async checkPermissions() {
+        const { speechRecognition, audioRecording } = await SpeechRecognition.checkPermissions();
 
-    console.log("RET", speechRecognition, audioRecording);
+        console.log("RET", speechRecognition, audioRecording);
 
-    if (speechRecognition !== 'granted' || audioRecording !== 'granted') {
-      await SpeechRecognition.requestPermissions({
-        permissions: ['audioRecording', 'speechRecognition'],
-      });
+        if (speechRecognition !== 'granted' || audioRecording !== 'granted') {
+            await SpeechRecognition.requestPermissions({
+                permissions: ['audioRecording', 'speechRecognition'],
+            });
+        }
     }
-  }
+
+    async music() {
+        await NativeAudio.preload({
+            assetId: 'bg',
+            assetPath: 'public/assets/sounds/test.mp3',
+            audioChannelNum: 1,
+            isUrl: false,
+            volume: 0.6
+        });
+
+        await NativeAudio.loop({ assetId: 'bg' });
+
+    }
+
+    recognize(): void {
+        this.startListening((data) => {
+            alert("Sentence: " + data);
+        });
+    }
+
+    async startListening(cb: (data: string) => void) {
+        await this.checkPermissions();
 
 
-  recognize(): void {
-    this.startListening((data) => {
-      alert("Sentence: " + data);
-    });
-  }
+        const { audioRecording, speechRecognition } = await SpeechRecognition.requestPermissions({
+            permissions: ['audioRecording', 'speechRecognition'],
+        });
 
-  async startListening(cb: (data: string) => void) {
-    await this.checkPermissions();
+        //    const langs =  await SpeechRecognition.getLanguages();
+        console.log("audioRecording, speechRecognitio", audioRecording, speechRecognition)
+        //  console.log("LANGS", langs);
 
+        console.log("🎙️ Spracherkennung wird gestartet...");
 
-    const { audioRecording, speechRecognition } = await SpeechRecognition.requestPermissions({
-      permissions: ['audioRecording', 'speechRecognition'],
-    });
+        SpeechRecognition.startListening({
+            language: "de-DE",
+            silenceThreshold: 800
+        });
 
-    //    const langs =  await SpeechRecognition.getLanguages();
-    console.log("audioRecording, speechRecognitio", audioRecording, speechRecognition)
-    //  console.log("LANGS", langs);
+        SpeechRecognition.addListener("result", async (event) => {
+            console.log("EVENT RESULT", event);
 
+            if (event.result) {
+                const finalSentence = event.result;
 
-    console.log("🎙️ Spracherkennung wird gestartet...");
+                await this.destroyRecognition();
+                cb(finalSentence);
+            }
+        });
 
-    SpeechRecognition.startListening({
-      language: "de-DE",
-      silenceThreshold: 800
-    });
+        // 🎧 Listener für Fehler
+        SpeechRecognition.addListener("error", (event) => {
+            console.error("❌ Spracherkennungsfehler:", event.message);
+        });
+    }
 
-    SpeechRecognition.addListener("result", async (event) => {
-      console.log("EVENT RESULT", event);
+    async destroyRecognition(): Promise<void> {
+        await SpeechRecognition.stopListening();
+        await SpeechRecognition.removeAllListeners();
+    }
 
-      if (event.result) {
-        const finalSentence = event.result;
+    async activateAudioSession() {
+        await SpeechSynthesis.activateAudioSession({ category: AudioSessionCategory.Ambient });
+    }
 
-        await this.destroyRecognition();
-        cb(finalSentence);
-      }
-    });
-
-    // 🎧 Listener für Fehler
-    SpeechRecognition.addListener("error", (event) => {
-      console.error("❌ Spracherkennungsfehler:", event.message);
-    });
-  }
-
-  async destroyRecognition(): Promise<void> {
-    await SpeechRecognition.stopListening();
-    await SpeechRecognition.removeAllListeners();
-  }
-
-
-  async speak() {
-    await SpeechSynthesis.initialize();
-    const { utteranceId } = await SpeechSynthesis.speak({
-      language: 'en-US',
-      pitch: 1.0,
-      queueStrategy: QueueStrategy.Add,
-      rate: 1.0,
-      text: 'Hello, World!',
-      voiceId: 'com.apple.ttsbundle.Samantha-compact',
-      volume: 1.0,
-    });
-  }
+    async speak() {
+        await SpeechSynthesis.initialize();
+        const { utteranceId } = await SpeechSynthesis.speak({
+            language: 'en-US',
+            pitch: 1.0,
+            queueStrategy: QueueStrategy.Add,
+            rate: 1.0,
+            text: 'Hello, World!',
+            voiceId: 'com.apple.ttsbundle.Samantha-compact',
+            volume: 1.0,
+        });
+    }
 }
